9eb667b405ab2aa8a798b58077a1664e520fa880
ApeironDominus <44783179+ApeironDominus@users.noreply.github.com>
2018-11-12 12:07:05 -0600

    Add files via upload
    Uploaded the initial file, currently it only contains the unfinished variable section.

---
+++ Final Project.py
@@ -0,0 +1,52 @@
+#Custom Car Shop
+#Daniel Causey
+#CIS-120-102-FA18
+
+#Define Variables
+totalCost = 0
+#Engine Variables
+vtwinEngineCost = 200
+v4EngineCost = 400
+v6EngineCost = 1000
+v8EngineCost = 1600
+v12EngineCost = 2500
+v16EngineCost = 10000
+smallElectricMotor = 500
+medElectricMotor = 1000
+largeElectricMotor = 2000
+nuclearReactor = 250000
+#Chassis Variables
+motorcycle = 1200
+sedan = 2000
+sports = 4000
+sportsutility = 6000
+truck = 10000
+luxury = 20000
+militaryGrade = 40000
+#Paintjob [multiply patterns by paint type]
+solid = 200
+metallic = 500
+pearlescent = 2000
+matte = 3000
+carbonFiber = 10000
+multicolored = 1.5
+camo = 2
+racingStripe = 1.3
+customDecal = 3
+#Interior
+fabric = 500
+pLeather = 1000
+standardLeather = 2500
+luxuryLeather = 10000
+#Extras
+vibrationSeats = 2000
+upgradedSpeakers = 2500
+luxurySpeakers = 8000
+powerSteering = 1000
+armoredPlating = 25000
+bulletresistantWindows = 10000
+nitro = 5000
+extraStorage = 4000
+
+
+
